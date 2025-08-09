b39632a9afecd133986e7eaa71440323d31af8e5
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-09 22:59:25 +0900

    ABC418 B問題を修正

--- ABC418/B.py
+++ ABC418/B.py
@@ -1,15 +1,18 @@
 s = input()
 ans = 0
 for i in range(len(s) - 2):
+    if s[i] != 't':
+        continue
     for j in range(len(s) - 1, i + 1, -1):
-        if s[i] == s[j] == 't':
-            x = 0
-            len_t = 0
-            for k in range(i, j + 1):
-                if s[k] == 't':
-                    x += 1
-                len_t += 1
-            p = (x - 2) / (len_t - 2)
-            if p >= ans:
-                ans = p
+        if s[j] != 't':
+            continue
+        x = 0
+        len_t = 0
+        for k in range(i, j + 1):
+            if s[k] == 't':
+                x += 1
+            len_t += 1
+        p = (x - 2) / (len_t - 2)
+        if p >= ans:
+            ans = p
 print(ans)
