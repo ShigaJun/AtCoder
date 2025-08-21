475ad5ff177327126460078ebb46419c3c32e8f6
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-22 00:44:33 +0900

    RLE（ランレングス圧縮）を関数化してシンプルにした（ADT_2025-08-21-20:30 E問題）

--- ADT_2025-08-21-20:30/E.py
+++ ADT_2025-08-21-20:30/E.py
@@ -1,31 +1,23 @@
 S = input()
 T = input()
-S_list = []
-T_list = []
 
-cnt = 1
-for i in range(len(S) - 1):
-    if S[i] == S[i + 1]:
-        cnt += 1
-    else:
-        S_list.append([S[i], cnt])
-        cnt = 1
-S_list.append([S[-1], cnt])
+def rle(s):
+    ans = []
+    for c in s:
+        if ans and ans[-1][0] == c:
+            ans[-1][1] += 1
+        else:
+            ans.append([c, 1])
+    return ans
 
-cnt = 1
-for i in range(len(T) - 1):
-    if T[i] == T[i + 1]:
-        cnt += 1
-    else:
-        T_list.append([T[i], cnt])
-        cnt = 1
-T_list.append([T[-1], cnt])
+s_rle = rle(S)
+t_rle = rle(T)
 
-if len(S_list) != len(T_list):
+if len(s_rle) != len(t_rle):
     print("No")
     exit()
-for i in range(len(T_list)):
-    if S_list[i][0] != T_list[i][0] or T_list[i][1] < S_list[i][1] or (T_list[i][1] > S_list[i][1] and S_list[i][1] == 1):
+for si, ti in zip(s_rle, t_rle):
+    if si[0] != ti[0] or ti[1] < si[1] or (ti[1] > si[1] and si[1] == 1):
         print("No")
         break
 else:
