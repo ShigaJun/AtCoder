4f1655c1b2d4b3baa548d46eb068cc63fd591499
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-23 00:20:05 +0900

    ABC419のB問題にコメントを追加

--- ABC/ABC419/B.py
+++ ABC/ABC419/B.py
@@ -1,14 +1,15 @@
 Q = int(input())
+# query は2次元リスト 例1：[[1, 6], [1, 7], [2], [1, 1], [2]]
 # リスト内包表記
 query = [list(map(int, input().split())) for _ in range(Q)]
-# print(query)
-b = []
+# print(query) # コメントを外すと query の中身を確認できる
+b = [] # 空の袋
 
 for i in range(Q):
-    # queryのタイプを見る
-    if query[i][0] == 1:
-        b.append(query[i][1])
-    else:
-        print(min(b))
-        b.remove(min(b))
-    # print(b)
+    # query のタイプを見る
+    if query[i][0] == 1: # query タイプが 1 のとき
+        b.append(query[i][1]) # 整数 x が書かれたボールを 1 つ袋の中に入れる
+    else: # query タイプが 2 のとき
+        print(min(b)) # 袋に入っているボールのうち書かれている整数が最小のものを 1 つ出力する
+        b.remove(min(b)) # その整数を袋から取り出す
+    # print(b) # コメントを外すと b の中身を確認できる
