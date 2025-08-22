ec8ee40128806c1051954c730f91b95ed65f87bf
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-22 14:15:25 +0900

    ABC419のC問題を解いた

--- ABC419/C.py
+++ ABC419/C.py
@@ -1,10 +1,14 @@
 import math
-import numpy as np
 
 n = int(input())
-r, c = [0] * n
+r = [0] * n
+c = [0] * n
 for i in range(n):
-    r[i], c[i] = map(int, int(input()))
+    r[i], c[i] = map(int, input().split())
 center_x = math.ceil((min(r) + max(r)) / 2)
 center_y = math.ceil((min(c) + max(c)) / 2)
+ans = 0
 for i in range(n):
+    chebyshev = max(abs(center_x - r[i]), abs(center_y - c[i]))
+    ans = max(chebyshev, ans)
+print(ans)
