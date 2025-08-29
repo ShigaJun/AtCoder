6aceae8995b5985434160ecb655e52b62cc28dd2
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-30 05:28:15 +0900

    ABC420のC問題を解説読んで修正した

--- ABC/ABC420/C.py
+++ ABC/ABC420/C.py
@@ -1,19 +1,17 @@
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-min_list = []
+ans = 0
 
 for i in range(N):
-    min_list.append(min(A[i], B[i]))
-ans = sum(min_list)
-for i in range(Q):
+    ans += min(A[i], B[i])
+for _ in range(Q):
     c, X, V = input().split()
     X, V = int(X), int(V)
+    ans -= min(A[X - 1], B[X - 1]) # 一度，対象インデックスの min を引いておく
     if c == 'A':
         A[X - 1] = V
     else:
         B[X - 1] = V
-    diff = min(A[X - 1], B[X - 1]) - min_list[X - 1]
-    min_list[X - 1] = min(A[X - 1], B[X - 1])
-    ans += diff
+    ans += min(A[X - 1], B[X - 1]) # 更新後の対象インデックスの min を足す
     print(ans)
