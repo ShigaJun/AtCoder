2ddcee5977a16954aba5fa2a69e4bbdd16556c16
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-30 21:40:59 +0900

    ABC420のB問題を解説を読んで解き直した

--- ABC/ABC420/B.py
+++ ABC/ABC420/B.py
@@ -1,31 +1,22 @@
 N, M = map(int, input().split())
-S = [input() for _ in range(N)]
-S = [list(map(int, str(s))) for s in S]
-results = [-1] * M
-ans = [0] * N
+S = [list(input()) for _ in range(N)]
 
-for i in range(M):
-    cnt_0 = 0
-    cnt_1 = 0
-    for j in range(N):
-        if S[j][i] == 0:
+pt = [0] * N # それぞれの人のポイントを格納した配列 [0, 0, 0]
+for j in range(M):
+    cnt_0 = cnt_1 = 0
+    for i in range(N):
+        if S[i][j] == '0':
             cnt_0 += 1
         else:
             cnt_1 += 1
-    if cnt_0 == 0:
-        results[i] = 1
-    elif cnt_1 == 0:
-        results[i] = 0
-    elif cnt_0 < cnt_1:
-        results[i] = 0
-    else:
-        results[i] = 1
-
-for i in range(M):
-    for j in range(N):
-        if S[j][i] == results[i]:
-            ans[j] += 1
-
-for i in range(N):
-    if ans[i] == max(ans):
-        print(i + 1, end=' ')
+    # 点数追加
+    for i in range(N):
+        if S[i][j] == '0':
+            if cnt_0 < cnt_1:
+                pt[i] += 1
+        else:
+            if cnt_0 > cnt_1:
+                pt[i] += 1
+    high = max(pt)
+    ans = [str(i + 1) for i in range(N) if pt[i] == high]
+print(" ".join(ans))
