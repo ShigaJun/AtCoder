63b523f7465c9ddea04ec1206afdca9b3ff07584
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-08-31 02:40:07 +0900

    ABC421のC問題を解説を読んで解いた

--- ABC/ABC421/C.py
+++ ABC/ABC421/C.py
@@ -1,2 +1,15 @@
 N = int(input())
 S = input()
+X = []
+
+for i in range(2 * N):
+    if S[i] == 'A':
+        X.append(i)
+
+sum_a = sum_b = 0
+for k in range(N):
+    sum_a += abs(X[k] - 2 * k)
+    sum_b += abs(X[k] - (2 * k + 1))
+
+ans = min(sum_a, sum_b)
+print(ans)
