9941134d185c4ccc95fe355fbcb2fd7c3b411df6
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-09-25 22:40:27 +0900

    ABC417 C問題を解説を読んで解いた

--- ABC/ABC417/C.py
+++ ABC/ABC417/C.py
@@ -1,3 +1,7 @@
+from collections import Counter
+
 N = int(input())
 A = list(map(int, input().split()))
 
+Ai_plus_i = Counter(A[i] + i for i in range(N))
+print(sum(Ai_plus_i[j - A[j]] for j in range(N)))
