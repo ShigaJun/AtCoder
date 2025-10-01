c9929d01f33bd506a92a7648bf5ec63f9b083169
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-10-01 16:17:39 +0900

    ABC412 C問題を解説読んで解き直した

--- ABC/ABC412/C.py
+++ ABC/ABC412/C.py
@@ -1,21 +1,25 @@
-t = int(input())
-for i in range(t):
-    n = int(input())
-    s = list(map(int, input().split()))
-    s = [s[0]] + sorted(s[1:-1]) + [s[-1]]
-    
+def solve():
+    N = int(input())
+    S = list(map(int, input().split()))
+    cur = S[0]
+    goal = S[-1]
+    S = sorted(S[1:-1])
     ans = 2
-    i = 0
-    j = 0
-    while j < n - 2:
-        j += 1
-        if s[i] * 2 >= s[-1]:
+
+    for i in range(len(S)):
+        if goal <= cur * 2:
             break
-        elif s[j + 1] > s[i] * 2:
-            if s[j] > s[i] * 2:
-                ans = -1
-                break
-            else:
-                i = j
-                ans += 1
-    print(ans)
+        if S[i] <= cur * 2 and (i == len(S) - 1 or S[i + 1] > cur * 2):
+            ans += 1
+            cur = S[i]
+        if S[i] > cur * 2:
+            break
+
+    if goal > cur * 2:
+        print(-1)
+    else:
+        print(ans)
+    return
+
+for _ in range(int(input())):
+    solve()
