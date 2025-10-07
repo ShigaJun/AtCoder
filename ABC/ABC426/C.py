819c94237c84e34b330d48be49f073880347c0ef
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-10-08 00:17:55 +0900

    ABC426 C問題を解説を読んで解いた

--- ABC/ABC426/C.py
+++ ABC/ABC426/C.py
@@ -1,6 +1,13 @@
 N, Q = map(int, input().split())
 
-OS = [x for x in range(1, N + 1)]
+OS = [1] * N
+oldest = 1
 for _ in range(Q):
     X, Y = map(int, input().split())
-    OS[Y - 1] += OS[X - 1]
+    sum_old = 0
+    for i in range(oldest, X + 1):
+        sum_old += OS[oldest - 1]
+        OS[oldest - 1] = 0
+        oldest += 1
+    print(sum_old)
+    OS[Y - 1] += sum_old
