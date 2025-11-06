68ab931cdcb5e4512a877dad676e306863c9ecaa
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2025-11-07 00:33:13 +0900

    ADT_20251021_3 の G 問題に解答

--- ADT/ADT_20251021_3/G.py
+++ ADT/ADT_20251021_3/G.py
@@ -1,10 +1,22 @@
+import collections
+
 T = int(input())
 
 for _ in range(T):
     N = int(input())
     A = list(map(int, input().split()))
 
-    if :
-        print("Yes")
+    c = collections.Counter(A)
+    if len(list(c)) == 2:
+        if list(c)[0] / list(c)[1] == -1 and abs(c[list(c)[0]] - c[list(c)[1]]) <= 1:
+            print("Yes")
+            continue
+    
+    sorted_A = sorted(A, key=abs)
+    
+    for i in range(1, N - 1):
+        if pow(sorted_A[i], 2) != sorted_A[i - 1] * sorted_A[i + 1]:
+            print("No")
+            break
     else:
-        print("No")
+        print("Yes")
