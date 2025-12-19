ec4e030fd899ea84ed8f8b18495334f7cd6c10e6
Watanabe Haruto <watanabe.haruto.4869@gmail.com>
2025-12-19 20:47:54 +0900

    ADT20251218_2 E 問題をより簡単な解法で解き直した

--- ADT/ADT_20251218_2/E.py
+++ ADT/ADT_20251218_2/E.py
@@ -1,23 +1,7 @@
-S = input()
-# "WA" の1つ前が  'W' だった場合，"WA" が新たに現れることになる．
-# 'A' の前に 'W' が何個あるかで最終的な文字列がわかる
-# 'W' * 3 + 'A' の場合 -> 'A' + 'C' * 3
-ans = []
-i = 0
-while i < len(S):
-    cnt = 0
-    if S[i] == 'W':
-        cnt += 1
-        i += 1
-        while i < len(S) and S[i] == 'W':
-            cnt += 1
-            i += 1
-        if i < len(S) and S[i] == 'A':
-            ans.append('A' + 'C' * cnt)
-            i += 1
-        else:
-            ans.append('W' * cnt)
-    else:
-        ans.append(S[i])
-        i += 1
-print(''.join(ans))
+S = list(input())
+#後ろから見ていけば簡単！
+for i in range(len(S) - 1, -1, -1):
+    if i - 1 >= 0 and S[i - 1] + S[i] == "WA":
+        S[i - 1] = 'A'
+        S[i] = 'C'
+print(''.join(S))
