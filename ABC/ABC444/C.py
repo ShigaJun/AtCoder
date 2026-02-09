8fff5047d5da88e84a1e8bb6cc17ef18e3e862ad
Watanabe Haruto <watanabe.haruto.4869@gmail.com>
2026-02-10 02:39:15 +0900

    ABC444 の C 問題の解き直し

--- ABC/ABC444/C.py
+++ ABC/ABC444/C.py
@@ -1,13 +1,22 @@
-N = int(input())
-A = list(map(int, input().split()))
+def solve(A):
+    L = min(A) + max(A)
+    for i in range(1, int(len(A) / 2)):
+        if A[i] + A[-(i + 1)] != L:
+            return False
+    else:
+        return True
 
-minL = max(A)
-listL = []
-if A[0] >= minL:
-    listL.append(A[0])
-for l in  A[1:]:
-    tmp = A[0] + l
-    if tmp >= minL:
-        listL.append(tmp)
-listL = sorted(set(listL))
-print(listL)
+N = int(input())
+A = sorted(list(map(int, input().split())))
+ans = []
+
+if N % 2 == 1:
+    ans.append(max(A))
+else:
+    if solve(A): ans.append(min(A) + max(A))
+    L = max(A)
+    A = [a for a in A if a != L]
+    if len(A) % 2 == 0:
+        if len(A) == 0 or (solve(A) and min(A) + max(A) == L):
+            ans.append(L)
+print(*sorted(ans))
